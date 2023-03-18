f629e438a23a15a5986fa90d7069f4e6272e5736
overpathz <overpathz@gmail.com>
2023-03-18 11:19:28 +0200

    Changes related to PR review [logging, custom exceptions, tests]

--- src/main/java/org/hoverla/bibernate/exception/ExceptionMessages.java
+++ src/main/java/org/hoverla/bibernate/exception/ExceptionMessages.java
@@ -4,4 +4,11 @@ public interface ExceptionMessages {
     // datasource
     String DATA_SOURCE_NOT_FOUND_MSG = "Cannot configure appropriate datasource. " +
             "Please, recheck your configuration. Provider that is specified in your configuration is %s";
+
+    // transaction
+    String TRANSACTION_ALREADY_BEGUN = "Transaction has already begun";
+    String TRANSACTION_BEGIN_ERROR = "Could not begin transaction";
+    String TRANSACTION_ROLLBACK_ERROR = "Could not rollback transaction";
+    String TRANSACTION_COMMIT_ERROR = "Could not commit transaction";
+    String TRANSACTION_CLOSE_ERROR = "Could not close the connection in transaction";
 }
