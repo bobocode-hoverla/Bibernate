8bff20aa1abdd2f35564c9e43e74450dd1f4b956
Pavlo-Anton Prudyus <antonprudyus9747@gmail.com>
2023-04-07 16:48:50 +0300

    add docs and logging

--- src/main/java/org/hoverla/bibernate/demo/Customer.java
+++ src/main/java/org/hoverla/bibernate/demo/Customer.java
@@ -1,6 +1,5 @@
 package org.hoverla.bibernate.demo;
 
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.hoverla.bibernate.annotation.*;
@@ -9,25 +8,20 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @ToString
-@NoArgsConstructor
+@Setter
 @Entity
 @Table(name = "customer")
 public class Customer {
     @Id
     private Integer id;
-    @Setter
     @Column(name = "first_name")
     private String firstName;
-    @Setter
     @Column(name = "last_name")
     private String lastName;
-    @Setter
     @Column(name = "email")
     private String email;
-    @Setter
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-
     @OneToMany
     private List<Project> projects;
 }
